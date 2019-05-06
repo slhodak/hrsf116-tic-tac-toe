f3992a2ad1bce8d607238ce1d030fe14ae02bcd1
Sam Hodak <slhodak@gmail.com>
2019-05-06 16:14:23 -0700

    Reorganize file structure

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static('public'));
+
 const styles = path.join(__dirname, 'style.css');
 const mainHtml = path.join(__dirname, 'index.html');
+
 app.get('/', (req, res) => { 
   res.status(200);
   res.sendFile(mainHtml); 
-  // res.sendFile(styles);
+  res.sendFile(styles);
 });
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}!`) });
