e9c3acbd7f34db5fdae726aecf407c69407a3eb4
Sam Hodak <slhodak@gmail.com>
2019-05-06 16:54:27 -0700

    Layout style by grid

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
-
 app.use(express.static('public'));
 
-const mainHtml = path.join(__dirname, path.normalize('../'), 'public', 'index.html');
-console.log(path.normalize('../'));
 app.get('/', (req, res) => { 
   res.status(200);
-  // res.sendFile(mainHtml);
+  console.log('heard ya!');
+  res.end('heard ya!');
 });
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}!`) });
