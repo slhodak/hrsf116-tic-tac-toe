eac9d5c291fc90c7da39071190caebc748bb7a67
Sam Hodak <slhodak@gmail.com>
2019-05-06 11:12:19 -0700

    Click handlers for tiles

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // Event Listeners
-
+let tiles = document.getElementsByClassName('tile');
+Array.from(tiles).forEach(element => {
+  element.addEventListener('click', (e) => {
+    handleTileClick(e.target);
+  });
+});
 
 // Models
 //    check for game end conditions
-
+let Game = {
+  board : [
+      [],
+      [],
+      []
+    ],
+  turn: 'O',
+  switchTurn: function() {
+    Game.turn === 'O' ? Game.turn = 'X' : Game.turn = 'O';
+  }
+}
 
 // Views
 
 
 // Controllers
+let handleTileClick = function(target) {
+  Game.turn === 'O' ? target.innerText = '_O_' : target.innerText = '_X_';
+  Game.switchTurn();
+};
