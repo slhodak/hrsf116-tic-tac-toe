d48b892cba26a1f1ec6b92338b13d8bb1515c19a
Sam Hodak <slhodak@gmail.com>
2019-05-06 16:25:07 -0700

    Bugfix starting server

--- server/server.js
+++ server/server.js
@@ -3,15 +3,14 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-const styles = path.join(__dirname, 'style.css');
-const mainHtml = path.join(__dirname, 'index.html');
-
+// const styles = path.join(__dirname, path.normalize('../'), 'public', 'style.css');
+// const mainHtml = path.join(__dirname, path.normalize('../'), 'public', 'index.html');
+console.log(path.normalize('../'));
 app.get('/', (req, res) => { 
   res.status(200);
-  res.sendFile(mainHtml); 
-  res.sendFile(styles);
+  res.sendFile(mainHtml);
 });
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}!`) });
