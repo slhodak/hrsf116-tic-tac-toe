e89b7e7731177a7194ff59f91417001fdaf14042
Sam Hodak <slhodak@gmail.com>
2019-05-06 11:24:17 -0700

    Update model logic with end conditions

--- app.js
+++ app.js
@@ -10,21 +10,49 @@ Array.from(tiles).forEach(element => {
 //    check for game end conditions
 let Game = {
   board : [
-      [],
-      [],
-      []
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
     ],
   turn: 'O',
   switchTurn: function() {
     Game.turn === 'O' ? Game.turn = 'X' : Game.turn = 'O';
+  },
+  updateBoard: function() {
+
+    checkForEndConditions((none, result) => {
+      if (none) {
+        refreshBoard();
+        return;
+      }
+      
+      if (result === 'o') {
+        displayResultMessage('O wins!');
+      } else if (result === 'x') {
+        displayResultMessage('X wins!');
+      } else {
+        displayResultMessage('It\'s a draw.');
+      }
+    });
+  },
+  checkForEndConditions: function() {
+
   }
 }
 
 // Views
+let refreshBoard = function() {
+
+};
+
+let displayResultMessage = function(message) {
+  
+};
 
 
 // Controllers
 let handleTileClick = function(target) {
-  Game.turn === 'O' ? target.innerText = '_O_' : target.innerText = '_X_';
+  // Game.turn === 'O' ? target.innerText = '_O_' : target.innerText = '_X_';
+
   Game.switchTurn();
 };
