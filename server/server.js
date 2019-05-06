a844b3214e2c3d8350b4df175d639f2937c4b363
Sam Hodak <slhodak@gmail.com>
2019-05-06 16:26:50 -0700

    Enable serving static public files

--- server/server.js
+++ server/server.js
@@ -3,14 +3,13 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// const styles = path.join(__dirname, path.normalize('../'), 'public', 'style.css');
-// const mainHtml = path.join(__dirname, path.normalize('../'), 'public', 'index.html');
+const mainHtml = path.join(__dirname, path.normalize('../'), 'public', 'index.html');
 console.log(path.normalize('../'));
 app.get('/', (req, res) => { 
   res.status(200);
-  res.sendFile(mainHtml);
+  // res.sendFile(mainHtml);
 });
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}!`) });
